c2ba531eca0c748d16867ff264c2b29e2e1a5189
jinhan_mac <jinhan38@naver.com>
2023-08-31 14:40:07 +0900

    코드 수정

--- app/src/main/java/com/example/findlocation/MainActivity.kt
+++ app/src/main/java/com/example/findlocation/MainActivity.kt
@@ -6,16 +6,7 @@ import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
-import com.example.findlocation.ui.theme.FindLocationTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -32,19 +23,7 @@ class MainActivity : ComponentActivity() {
                 10
             )
         }
-        setContent {
-            FindLocationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
     }
-
 
     override fun onResume() {
         super.onResume()
@@ -70,20 +49,4 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FindLocationTheme {
-        Greeting("Android")
-    }
 }
