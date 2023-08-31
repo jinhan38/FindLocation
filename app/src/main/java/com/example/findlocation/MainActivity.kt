2196a00de94e449e307dc87b01a9c4dd8d4a5014
jinhan_mac <jinhan38@naver.com>
2023-08-31 14:38:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/findlocation/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.findlocation
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import com.example.findlocation.ui.theme.FindLocationTheme
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                10
+            )
+        }
+        setContent {
+            FindLocationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        getLocation()
+    }
+
+    private fun getLocation() {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        val lm = getSystemService((LOCATION_SERVICE)) as LocationManager
+
+        lm.requestLocationUpdates(
+            LocationManager.NETWORK_PROVIDER, 1L, 1000.0f
+        ) {
+            Log.d("TAG", "getLocation: it : $it")
+        }
+    }
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FindLocationTheme {
+        Greeting("Android")
+    }
+}
